0ffbaf9d77624f164c1d1d790b98d86f3b1092cd
Abhinav Mathur <mathur.abhinav1108@gmail.com>
2025-11-18 22:39:25 +0530

    Product list page done

--- src/pages/productlist/Index.jsx
+++ src/pages/productlist/Index.jsx
@@ -1,44 +1,17 @@
 import Banner from "@/components/Banner";
-import AuthLayout from "../common/AuthLayout";
-import Slider1 from "../../Assets/Images/Slider1.png";
-import ProductGrid from "@/components/ProductGrid";
+import ProductListBanner from "../../Assets/Images/ProductListBanner.png";
+import Layout from "../common/Layout";
+import ProductGrid from "./ProductGrid.jsx";
 
 function Index() {
-    const sampleProducts = [
-        {
-            id: 1,
-            title: "BELMONT DEEP-SEAT CONTEMPORARY SOFA",
-            price: "₹68,500",
-            image: Sliderimage2?.src
-
-        },
-        {
-            id: 2,
-            title: "MONARCH BRUSHED-BRASS ARCHED FLOOR LAMP",
-            price: "₹22,300",
-            image: Sliderimage?.src
-            ,
-        },
-        {
-            id: 3,
-            title: "HERITAGE HANDWOVEN TEXTURED UPHOLSTERY CUSHION SET",
-            price: "₹35,750",
-            image: Sliderimage1?.src
-        },
-        {
-            id: 4,
-            title: "AURELUM LARGE-FORM SCULPTED CERAMIC CENTERPIECE VASE",
-            price: "₹85,000",
-            image: Sliderimage3?.src
-
-        },
-    ];
-    return (<>
-        <AuthLayout>
-            <Banner Slider1={Slider1} />
-            <ProductGrid products={sampleProducts} title={"Our Best Seller"}/>
-        </AuthLayout>
-    </>);
+    return (
+        <Layout>
+            <Banner Slider1={ProductListBanner} 
+            title={"From Blueprint to Reality — Complete AND LUXURY FURNITURE"} 
+            button={"SHOP OUR FURNITURE"}/>
+            <ProductGrid/>
+        </Layout>
+    );
 }
 
 export default Index;
