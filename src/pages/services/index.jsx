bbaa50d27c4aef0b259992a9385d3e9188c88f9b
ankitjain0748 <ankitjain0748@gmail.com>
2025-12-14 15:52:17 +0530

    manags

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -33,7 +33,6 @@ export default function Index() {
                     </div>
                 </div>
             </div>
-
             <Predictable />
             <ResidentialDesign />
             <CommercialDesign />
