d3151920232f2527745725a209cc79c8da4ae2f3
Abhinav Mathur <mathur.abhinav1108@gmail.com>
2025-12-18 22:33:07 +0530

    Updated token and context

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Index from "./home/Index";
 import Layout from "./common/Layout";
-import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
     <Layout>
-      <Toaster />
       <Index />
     </Layout>
   );
