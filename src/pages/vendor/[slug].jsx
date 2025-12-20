bdd543985776a83bb82260657ad2661f5faba5f1
ankitjain0748 <ankitjain0748@gmail.com>
2025-12-20 22:36:08 +0530

    manages  daat

--- src/pages/vendor/[slug].jsx
+++ src/pages/vendor/[slug].jsx
@@ -1,13 +1,23 @@
 import Banner from "@/components/Banner";
 import ProductListBanner from "../../Assets/Images/ProductListBanner.png";
 import Layout from "../common/Layout";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 export default function Index() {
+    const router  = useRouter();
+console.log("router" ,router.query.slug)
+const slug=  router.query.slug;
+
+useEffect(()=>{
+    
+
+},[slug])
 
     return (
         <Layout>
             <Banner Slider1={ProductListBanner}
                 title={"From Blueprint to Reality — Complete Interior Design & Execution"}
-                button={"SHOP OUR FURNITURE"} />
+               />
 
 
         </Layout>
